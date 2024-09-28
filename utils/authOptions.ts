d535462b55796c9a71b08203a41273c11aafd579
Aditya Raj <adiabhiraj141@gmail.com>
2024-09-28 09:30:05 +0530

    push all

---
+++ utils/authOptions.ts
@@ -0,0 +1,57 @@
+import { AuthOptions, Profile, Session } from "next-auth";
+// import GoogleProvider from "next-auth/providers/google";
+import GithubProvider from "next-auth/providers/github";
+
+import connectDB from "@/config/database";
+import User from "@/models/User";
+
+export const authOptions: AuthOptions = {
+  providers: [
+    // GoogleProvider({
+    //   clientId: process.env.GOOGLE_CLIENT_ID as string,
+    //   clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+    //   authorization: {
+    //     params: {
+    //       prompt: "consent",
+    //       access_type: "offline",
+    //       response_type: "code",
+    //     },
+    //   },
+    // }),
+    GithubProvider({
+      clientId: process.env.GITHUB_CLIENT_ID as string,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET as string,
+      authorization: {
+        params: {
+          prompt: "consent",
+          access_type: "offline",
+          response_type: "code",
+        },
+      },
+    }),
+  ],
+  secret: process.env.NEXTAUTH_SECRET,
+  callbacks: {
+    async signIn({ profile }: { profile?: Profile }) {
+      await connectDB();
+      const userExists = await User.findOne({ email: profile?.email });
+      if (!userExists) {
+        const username = profile?.name?.slice(0, 20);
+        await User.create({
+          email: profile?.email,
+          username,
+          image: profile?.image,
+        });
+      }
+      return true;
+    },
+    async session({ session }: { session: Session }) {
+      if (session.user) {
+        await connectDB();
+        const user = await User.findOne({ email: session.user.email });
+        session.user.id = user?._id.toString() as string;
+      }
+      return session;
+    },
+  },
+};
